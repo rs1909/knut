421dabbbb8de3dfc0d8b20cef10764e5656fbf0d
Robert Szalai <robicjedi@gmail.com>
2005-10-01 14:11:08 +0000

    pderror.[c,h]

---
+++ src/pderror.h
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------------- //
+//
+// This is part of PDDE-CONT
+// Copyright (c) 2002, 2003, 2004, 2005 by Robert Szalai
+//
+// For license, see the file COPYING in the package's root directory
+//
+// ------------------------------------------------------------------------- //
+
+#ifndef ERROR_H
+#define ERROR_H
+
+void PDError( int i );
+
+#endif
