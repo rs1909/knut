67fd0b9daccd59376bce9134d4c787488b7833da
Robert Szalai <robicjedi@gmail.com>
2006-01-03 13:26:41 +0000

    touch configure.ac in regen.sh

--- pdde-tf/regen.sh
+++ pdde-tf/regen.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 if ! `test -e ChangeLog`; then touch ChangeLog ; fi
 svn log -r HEAD -q | sed -e s/' |.*.|'// -e s/' (.*)'// -e s/' '/'\\ '/g -e /'---'/d >REVISION
+touch configure.ac
 aclocal
 automake -a -c
 autoconf
