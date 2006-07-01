1b9cd01e850889d4c4c1f593ea85138f129b06d4
Robert Szalai <robicjedi@gmail.com>
2006-07-01 16:21:03 +0000

    A new CMake based build system

---
+++ pdde-tf/src/config.h.cmake
@@ -0,0 +1,11 @@
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@PACKAGE_NAME@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@PACKAGE_VERSION@"
+
+/* "SVN REVISION" */
+#define PACKAGE_REVISION "@PACKAGE_REVISION@"
+
+/* DEBUG has to be turned on? */
+#cmakedefine DEBUG
