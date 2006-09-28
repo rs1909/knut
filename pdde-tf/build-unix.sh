d7609c50c4efe8c6798b8af93f23f02d0612d051
Robert Szalai <robicjedi@gmail.com>
2006-09-28 23:36:06 +0000

    building in a separate directory

--- pdde-tf/build-unix.sh
+++ pdde-tf/build-unix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SCRIPTDIR=${0/build-unix.sh/}
-cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=$HOME/pdde-cont -D CMAKE_BUILD_TYPE=RELEASE $SCRIPTDIR
-make -C $SCRIPTDIR
-make install -C $SCRIPTDIR
+# SCRIPTDIR=${0/build-unix.sh/}
+cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=$HOME/pdde-cont -D CMAKE_BUILD_TYPE=RELEASE `pwd`
+make
+make install
