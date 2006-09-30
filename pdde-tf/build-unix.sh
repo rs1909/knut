01061e00cfd4b52d449a9491f18cc56c725414c2
Robert Szalai <robicjedi@gmail.com>
2006-09-30 09:52:41 +0000

    build update

--- pdde-tf/build-unix.sh
+++ pdde-tf/build-unix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# SCRIPTDIR=${0/build-unix.sh/}
-cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=$HOME/pdde-cont -D CMAKE_BUILD_TYPE=RELEASE `pwd`
+SCRIPTDIR=${0/build-unix.sh/}
+cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=$HOME/pdde-cont -D CMAKE_BUILD_TYPE=RELEASE $SCRIPTDIR
 make
 make install
