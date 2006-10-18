8f7f29c92296dad869970eb1202ca2d00f418739
Robert Szalai <robicjedi@gmail.com>
2006-10-18 23:24:15 +0000

    The continuation parameter has to be set up after every Reset, otherwise it will be forgotten.

--- pdde-tf/cli/main.cpp
+++ pdde-tf/cli/main.cpp
@@ -219,19 +219,16 @@ int main( int argc, const char** argv )
 			{
 				std::cout<<"\nSwitching to the period two branch (TF).\n";
 				pt.SwitchTFPD( params->getDsStart() );
-				pt.setCont( params->getCp()-VarPAR0 );
 			}
 			else if( params->getBranchSW() == TFBRSwitch )
 			{
 				std::cout<<"\nSwitching to the other branch (TF).\n";
 				pt.SwitchTFLP( params->getDsStart());
-				pt.setCont( params->getCp()-VarPAR0 );
 			}
 			else if( params->getBranchSW() == TFHBSwitch )
 			{
 				std::cout<<"\nSwitching to the periodic solution branch at the HOPF point (TF).\n";
 				pt.SwitchTFHB( params->getDsStart() );
-				pt.setCont( params->getCp()-VarPAR0 );
 			}
 			else
 			{
@@ -240,6 +237,7 @@ int main( int argc, const char** argv )
 				pt.Tangent();
 			}
 			pt.Reset( eqn, var );
+			pt.setCont( params->getCp()-VarPAR0 );
 			
 			std::cout<<'\n';
 			int ustab = 0, ustabprev = 0;
