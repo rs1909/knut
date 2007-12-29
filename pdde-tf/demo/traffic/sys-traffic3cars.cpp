39a77fd25f5f79926256627cef10c778760a8f0c
Robert Szalai <robicjedi@gmail.com>
2007-12-29 21:48:09 +0000

    vectorized traffic3cars example

--- pdde-tf/demo/traffic/sys-traffic3cars.cpp
+++ pdde-tf/demo/traffic/sys-traffic3cars.cpp
@@ -32,73 +32,90 @@ int sys_npar() { return 5; }
 int sys_ntau() { return 2; }
 int sys_nderi() { return 0; }
 
-void sys_tau( Vector& out, double t, const Vector& par )
+void sys_p_tau( Array2D<double>& out, Array1D<double>& time, const Array1D<double>& par )
 {
-	out(0) = 0.0;
-	out(1) = par(2);
+  for (int idx = 0; idx < time.Size(); ++idx)
+  {
+    out(0,idx) = 0.0;
+    out(1,idx) = par(2);
+  }
 }
 
-void sys_dtau( Vector& out, double t, const Vector& par, int vp )
+void sys_p_dtau( Array2D<double>& out, Array1D<double>& time, const Array1D<double>& par, int vp )
 {
-	out(0) = 0.0;
-	if( vp == 2 ) out(1) = 1.0;
-	else out(1) = 0.0;
+  for (int idx = 0; idx < time.Size(); ++idx)
+  {
+    out(0,idx) = 0.0;
+    if( vp == 2 ) out(1,idx) = 1.0;
+    else out(1,idx) = 0.0;
+  }
 }
 
 static inline double V( double x )
 {
-	return pow( ( x - 1 ) / 1, 3.0 ) / ( pow( ( x - 1 ) / 1, 3.0 ) + 1);
+  return pow( ( x - 1 ) / 1, 3.0 ) / ( pow( ( x - 1 ) / 1, 3.0 ) + 1);
 }
 
-void sys_rhs( Vector& out, double t, const Matrix& yy, const Vector& par )
+void sys_p_rhs( Array2D<double>& out, Array1D<double>& time, const Array3D<double>& yy, const Array1D<double>& par )
 {
-#define xx(i,j) yy(i-1,j-1)
-#define f(i,j) out(i-1)
+#define xx(i,j) yy(i-1,j-1,idx)
+#define f(i,j) out(i-1,idx)
+  for (int idx = 0; idx < time.Size(); ++idx)
+  {
+    if ( xx(4,2) > 1 ){
+      f(1,1) = par(3) * ( par(4) * V(xx(4,2)) - xx(1,1) ); 
+    }else{
+      f(1,1) = - par(3) * xx(1,1);
+    }
+  }
 
-	if ( xx(4,2) > 1 ){
-		f(1,1) = par(3) * ( par(4) * V(xx(4,2)) - xx(1,1) ); 
-	}else{
-		f(1,1) = - par(3) * xx(1,1);
-	}
-	
-	if( xx(5,2) > 1 ){
-		f(2,1) = par(3) * ( par(4) * V(xx(5,2)) - xx(2,1) ); 
-	}else{
-		f(2,1) = - par(3) * xx(2,1);
-	}
-	
-	if( ( 3*par(1) - xx(5,2) - xx(4,2) ) > 1 ){
-		f(3,1) = par(3) * ( par(4) * V( 3*par(1) - xx(5,2) - xx(4,2) ) - xx(3,1) );
-	}else{
-		f(3,1) = - par(3) * xx(3,1);
-	}
-	
-	f(4,1) = xx(2,1) - xx(1,1);
-	f(5,1) = xx(3,1) - xx(2,1);
+  for (int idx = 0; idx < time.Size(); ++idx)
+  {
+    if( xx(5,2) > 1 ){
+      f(2,1) = par(3) * ( par(4) * V(xx(5,2)) - xx(2,1) ); 
+    }else{
+      f(2,1) = - par(3) * xx(2,1);
+    }
+  }
+
+  for (int idx = 0; idx < time.Size(); ++idx)
+  {
+    if( ( 3*par(1) - xx(5,2) - xx(4,2) ) > 1 ){
+      f(3,1) = par(3) * ( par(4) * V( 3*par(1) - xx(5,2) - xx(4,2) ) - xx(3,1) );
+    }else{
+      f(3,1) = - par(3) * xx(3,1);
+    }
+  }
+
+  for (int idx = 0; idx < time.Size(); ++idx)
+  {
+    f(4,1) = xx(2,1) - xx(1,1);
+    f(5,1) = xx(3,1) - xx(2,1);
+  }
 }
 
-void sys_deri( Matrix &out, double t, const Matrix& x, const Vector& par, 
-	       int nx, const int* vx, int np, const int* vp, const Matrix& vv )
+void sys_p_deri( Array3D<double>&, Array1D<double>& time, const Array3D<double>& yy, const Array1D<double>& par, 
+                 int nx, const int* vx, int np, const int* vp, const Array3D<double>& vv )
 {
 
 }
 
 void sys_stpar( Vector& par )
 {
-	par(0) = 1.1; // T the period length 
-	par(1) = 2.0; // L/N
-	par(2) = 1.0; // the delay
-	par(3) = 1.0; // alpha
-	par(4) = 1.0; // v_0
+  par(0) = 1.1; // T the period length 
+  par(1) = 2.0; // L/N
+  par(2) = 1.0; // the delay
+  par(3) = 1.0; // alpha
+  par(4) = 1.0; // v_0
 }
 
 void sys_stsol( Vector& out, double t )
 {
-	out(0) = 0.5;// + 1.3*sin(2*M_PI*t);
-	out(1) = 0.5;// + 1.3*(2*M_PI*t+2*M_PI/3);
-	out(2) = 0.5;// + 1.3*sin(2*M_PI*t+4*M_PI/3);
-	out(3) = 2.0;// - 0.5*cos(2*M_PI*t+2*M_PI/3);
-	out(4) = 2.0;// - 0.5*cos(2*M_PI*t+4*M_PI/3);
+  out(0) = 0.5;// + 1.3*sin(2*M_PI*t);
+  out(1) = 0.5;// + 1.3*(2*M_PI*t+2*M_PI/3);
+  out(2) = 0.5;// + 1.3*sin(2*M_PI*t+4*M_PI/3);
+  out(3) = 2.0;// - 0.5*cos(2*M_PI*t+2*M_PI/3);
+  out(4) = 2.0;// - 0.5*cos(2*M_PI*t+4*M_PI/3);
 }
 
 }
