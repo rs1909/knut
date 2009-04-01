8cdab8a89811f63ac19a278940bf1aab5e1bb06d
Robert Szalai <robicjedi@gmail.com>
2009-04-01 17:11:14 +0100

    Add debug build for Mac.

---
+++ pdde-tf/build-mac-debug.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+SCRIPTDIR=${0/build-mac-debug.sh/}
+cmake -G "Unix Makefiles"\
+	-D CMAKE_CXX_COMPILER="/usr/bin/g++-4.2"\
+	-D CMAKE_C_COMPILER="/usr/bin/gcc-4.2"\
+	-D CMAKE_OSX_ARCHITECTURES="x86_64"\
+	-D CMAKE_INSTALL_PREFIX=$HOME/Apps/Knut\
+	-D CMAKE_BUILD_TYPE=DEBUG $SCRIPTDIR
+make
+make install
