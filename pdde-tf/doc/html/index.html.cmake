a3c0e44b9bd14b935af49777fe799979a5ae47ae
Robert Szalai <robicjedi@gmail.com>
2009-04-03 17:45:47 +0100

    Webpage generation.

--- pdde-tf/doc/html/index.html.cmake
+++ pdde-tf/doc/html/index.html.cmake
@@ -3,7 +3,7 @@
     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title>KNUT</title>
+	<title>@PACKAGE_NAME@</title>
 	<style>h3.fn,span.fn { margin-left: 1cm; text-indent: -1cm; }
 		a:link { color: #2B5E82; text-decoration: none }
 		a:visited { color: #672967; text-decoration: none }
@@ -18,15 +18,21 @@
 <p>
 <table cellpadding="2" cellspacing="1" border="0" width="100%" bgcolor="#e5e5e5">
 	<tr>
-		<th bgcolor="#333366" style="color: white"> <h2>KNUT: A continuation and bifurcation software for delay-differential equations</h2></th>
+		<th bgcolor="#333366" style="color: white"> <h2>@PACKAGE_NAME@: A continuation and bifurcation software for delay-differential equations</h2></th>
 	</tr>
 </table>
 
 <p />
+<b>Note</b>
+<hr size="1" width="100%" color="black" />
+<p>
+This software is a new and renamed version of <a href="http://seis.bris.ac.uk/~rs1909/pdde/">PDDE-CONT</a>.
+</p>
+
 <b>Capabilities</b>
 <hr size="1" width="100%" color="black" />
 <p>
-KNUT can continue periodic solutions and periodic solution bifurcations of autonomous and periodically forced delay-differential equations that are in the general form</p>
+@PACKAGE_NAME@ can continue periodic solutions and periodic solution bifurcations of autonomous and periodically forced delay-differential equations that are in the general form</p>
 <p align="center">
 <i>dx(t)/dt = f(t, x(t-&tau;<sub>0</sub>), x(t-&tau;<sub>1</sub>), ..., x(t-&tau;<sub>r</sub>), &lambda;).</i>
 </p>
@@ -49,7 +55,7 @@ be continued by switching to the periodic solution branch.
 <b> Documentation </b>
 <hr size="1" width="100%" color="black" />
 <p>
-See the <a href="manual.pdf"> User's Manual </a>.
+See the <a href="manual.pdf">User's Manual</a>.
 </p>
 
 <b> Download </b>
@@ -60,43 +66,17 @@ See the <a href="manual.pdf"> User's Manual </a>.
 <tr style="color: white"><th bgcolor="#333366">Current version (@PACKAGE_VERSION@)</th> <th bgcolor="#333366"> Package </th>
 </tr>
 <tr>
-<td> Source code </td> <td> <a href="KNUT-@PACKAGE_VERSION@.tar.bz2"> KNUT-@PACKAGE_VERSION@.tar.bz2 </a></td>
+<td> Source code </td> <td> <a href="@PACKAGE_NAME@-@PACKAGE_VERSION@.tar.bz2"> @PACKAGE_NAME@-@PACKAGE_VERSION@.tar.bz2 </a></td>
 </tr>
 <tr>
-<td> Windows XP installer </td> <td> <a href="KNUT-@PACKAGE_VERSION@.exe"> KNUT-@PACKAGE_VERSION@.exe</a> (Install the <a href="http://prdownloads.sf.net/mingw/MinGW-5.0.3.exe?download">MinGW</a> g++ compiler, too.)</td></tr>
+<td> Mac Application (Intel x86_64) </td> <td> <a href="@PACKAGE_NAME@-@PACKAGE_VERSION@.dmg"> @PACKAGE_NAME@-@PACKAGE_VERSION@.dmg</a></td></tr>
 </table>
 </p><p/>
 
 <b>Installation from the source code</b>
 <hr size="1" width="100%" color="black" /><p>
-The software can be built using the cross-platform build facility <a href="http://www.cmake.org">CMake</a>. The build also requires a C and C++ compiler, preferably <a href="http://gcc.gnu.org">GCC</a>, and the <a href="http://math-atlas.sourceforge.net/">ATLAS</a> linear algebra library. The (optional) graphical user interface requires the <a href="http://www.trolltech.com/products/qt/downloads">Qt 4.2</a> GUI toolkit.
+The software can be built using the cross-platform build facility <a href="http://www.cmake.org">CMake</a>. The build also requires a C and C++ compiler, preferably <a href="http://gcc.gnu.org">GCC</a>, and the <a href="http://math-atlas.sourceforge.net/">ATLAS</a> linear algebra library. The (optional) graphical user interface requires the <a href="http://www.qtsoftware.com/products/qt/downloads">Qt</a> GUI toolkit.
 </p>
-<p>
-The compilers are usually present on Linux, BSD, Solaris or other Unix systems. For Windows the 
-<a href="http://www.mingw.org">MinGW</a> and MSYS environments can be used. Also the Microsoft Visual Studio C/C++ compiler together with NMake can be used, but it will build only the command line interface.
-</p>
-<p>
-The required ATLAS linear algebra library is available for different platforms in binary form. (The files were collected from different places across the internet.) If the binary for a particular computing platform is not found here, download the source from the 
-<a href="http://math-atlas.sourceforge.net/"> ATLAS web page</a> and follow the compilation instructions.
-</p>
-<p>
-<table cellpadding="2" cellspacing="1" border="0" width="100%" bgcolor="#F6F6F6">
-<tr style="color: white"><th bgcolor="#333366">Operating system, Processor</th> <th bgcolor="#333366"> Binary package </th>
-</tr>
-<tr>
-<td>Linux, Pentium 4 </td> <td> <a href="atlas3.6.0_Linux_P4SSE2.tar.gz"> atlas3.6.0_Linux_P4SSE2.tar.gz </a> </td></tr>
-<tr>
-<td>Linux, Pentium-M (Centrino) </td> <td> <a href="atlas3.6.0_Linux_PIIISSE2.tgz"> atlas3.6.0_Linux_PIIISSE2.tgz </a> </td></tr>
-<tr>
-<td>Windows NT, 2000, XP, Pentium 4 </td> <td> <a href="atlas3.6.0_WinNT_P4SSE2.tgz"> atlas3.6.0_WinNT_P4SSE2.tgz </a> </td></tr>
-<tr>
-<td>Solaris, UltraSparc II and newer </td> <td> <a href="atlas3.5.0beta_SunOS_SunUS2_2.tar.gz"> atlas3.5.0beta_SunOS_SunUS2_2.tar.gz  </a> </td>
-</tr>
-<tr>
-<td>Linux Itanium 2 </td> <td> <a href="atlas3.7.33-Linux_IA64.tar.gz">
-atlas3.7.33-Linux_IA64.tar.gz </a> </td> </tr>
-</table>
-</p><p />
 
 <b>License</b>
 <hr size="1" width="100%" color="black" />
@@ -115,10 +95,15 @@ can be distributed under the GNU Lesser General Public License <a href="http://w
 <b> Related publications </b>
 <hr size="1" width="100%" color="black" />
 <p>
-<a name="man">[1]</a> R. Szalai, <i> KNUT: A continuation and bifurcation software for 
+<a name="man">[1]</a> R. Szalai, <i> Knut: A continuation and bifurcation software for 
+delay-differential equations</i>, 2009.
+</p>
+<p>
+<a name="man">[2]</a> R. Szalai, <i> PDDE-CONT: A continuation and bifurcation software for 
 delay-differential equations</i>, 2005.
-</p><p>
-[2] R. Szalai, G. Stepan, and S. J. Hogan. 
+</p>
+<p>
+[3] R. Szalai, G. Stepan, and S. J. Hogan. 
 <i>Continuation of bifurcations in periodic delay-differential equations using characteristic matrices</i>.
 SIAM Journal on Scientific Computing, 28(4):1301-1317, 2006.
 </p>
