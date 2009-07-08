abba5671b12e75e68c9de49261d480bcc19dfaba
Robert Szalai <robicjedi@gmail.com>
2009-07-08 12:00:34 +0100

    Removed historic files after the 1.0.1 version.

--- matlab/ddemult.m
+++
@@ -1,7 +0,0 @@
-function mult = ddemult(point)
-    mult = [ ];
-    for i=1:length(point) 
-        mult(i,1:length(point(i).stability.mu))=point(i).stability.mu;
-    end
-
-end
--- regen.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-if ! `test -e ChangeLog`; then touch ChangeLog ; fi
-aclocal
-automake -a -c
-autoconf
