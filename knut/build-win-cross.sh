d6bc6e109588ae30f2c3a6aa6da800cabff2ea24
Robert Szalai <robicjedi@gmail.com>
2009-08-16 19:37:28 +0100

    Added VFGEN. It requires GiNaC.

--- knut/build-win-cross.sh
+++ knut/build-win-cross.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 SCRIPTDIR=${0/build-win-cross.sh/}
 rm -f CMakeCache.txt
-cmake -DCMAKE_TOOLCHAIN_FILE=$SCRIPTDIR/wintoolchain -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$HOME/WinKnut/ $SCRIPTDIR
+cmake -DCMAKE_TOOLCHAIN_FILE=$SCRIPTDIR/wintoolchain \
+	-DPKG_CONFIG_EXECUTABLE=`which mingw32-pkg-config` \
+	-DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_INSTALL_PREFIX=$HOME/WinKnut/ $SCRIPTDIR
 make
 make install
