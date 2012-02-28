e6c56a75a184c92ddc0b5fd7bc7490d7f5937997
Robert Szalai <robicjedi@gmail.com>
2012-02-28 17:30:05 +0000

    A compiler is no longer necessary. - sysname is now the source of the system definition which can be a c++,   vf or so file. - there is an xml attribut to sysname in the .knut file that specifies the   file type, it is called 'type'. - if the 'type' attribute is set to type="vf0", the vector field file won't   be compiled, but expression will be evaluated runtime.

---
+++ knut/build-mac-clang.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# CMAKE_OSX_ARCHITECTURES is necessary to remove other architectures
+SCRIPTDIR=${0/build-mac-clang.sh/}
+pushd $SCRIPTDIR
+if test -d ../.git ; then 
+	echo 'It is a git repository, updating REVISION' 
+	./mkrevision.sh
+	fi
+popd
+cmake -G "Unix Makefiles"\
+	-D CMAKE_CXX_COMPILER="clang++"\
+	-D CMAKE_C_COMPILER="clang"\
+	-D CMAKE_Fortran_COMPILER="gfortran"\
+	-D CMAKE_C_FLAGS="-m64 -g -O2 -W -Wall -Wno-unused -Wno-unknown-pragmas"\
+	-D CMAKE_CXX_FLAGS="-m64 -g -O2 -W -Wall -Wno-unused -Wconversion"\
+	-D CMAKE_Fortran_FLAGS="-m64 -g -O3"\
+	-D CMAKE_INSTALL_PREFIX=$HOME/Applications/Knut\
+	-D CMAKE_BUILD_TYPE=RELWITHDEBINFO $SCRIPTDIR
+make
