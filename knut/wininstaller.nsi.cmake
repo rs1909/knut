a3eb81696bdf19ea568ab849f1a26602d3f4587c
Robert Szalai <robicjedi@gmail.com>
2013-12-26 14:59:46 +0000

    Version 9. Fix Windows installer script.

--- knut/wininstaller.nsi.cmake
+++ knut/wininstaller.nsi.cmake
@@ -15,7 +15,7 @@
 
   ;Default installation folder
   InstallDir "$PROGRAMFILES\${PACKAGE_NAME}"
-  
+
   ;Get installation folder from registry if available
   InstallDirRegKey HKCU "Software\${PACKAGE_NAME}" ""
 
@@ -35,22 +35,22 @@
 
   !insertmacro MUI_PAGE_LICENSE "@CMAKE_SOURCE_DIR@\COPYING"
   !insertmacro MUI_PAGE_DIRECTORY
-  
+
   ;Start Menu Folder Page Configuration
-  !define MUI_STARTMENUPAGE_REGISTRY_ROOT "HKCU" 
-  !define MUI_STARTMENUPAGE_REGISTRY_KEY "Software\${PACKAGE_NAME}" 
+  !define MUI_STARTMENUPAGE_REGISTRY_ROOT "HKCU"
+  !define MUI_STARTMENUPAGE_REGISTRY_KEY "Software\${PACKAGE_NAME}"
   !define MUI_STARTMENUPAGE_REGISTRY_VALUENAME "Start Menu Folder"
-  
+
   !insertmacro MUI_PAGE_STARTMENU Application $STARTMENU_FOLDER
-  
+
   !insertmacro MUI_PAGE_INSTFILES
-  
+
   !insertmacro MUI_UNPAGE_CONFIRM
   !insertmacro MUI_UNPAGE_INSTFILES
 
 ;--------------------------------
 ;Languages
- 
+
   !insertmacro MUI_LANGUAGE "English"
 
 ;--------------------------------
@@ -59,7 +59,7 @@
 Section "Dummy Section" SecDummy
 
   SetOutPath "$INSTDIR"
-  
+
   ;ADD YOUR OWN FILES HERE...
   File "@CMAKE_SOURCE_DIR@\COPYING"
   SetOutPath "$INSTDIR\bin"
@@ -75,7 +75,7 @@ Section "Dummy Section" SecDummy
   File "@QtWidgetsLoc@"
   File "@QtPrintSupportLoc@"
   File "@QtSvgLoc@"
-  File "@QtCorePath@\icudt51.dll"  
+  File "@QtCorePath@\icudt51.dll"
   File "@QtCorePath@\icuin51.dll"
   File "@QtCorePath@\icuuc51.dll"
   SetOutPath "$INSTDIR\bin\platforms"
@@ -84,21 +84,21 @@ Section "Dummy Section" SecDummy
   SetOutPath "$INSTDIR"
   File /r "@CMAKE_INSTALL_PREFIX@\include"
   File /r "@CMAKE_INSTALL_PREFIX@\demo"
-  File /r "@CMAKE_INSTALL_PREFIX@\matlab"
-  
+  File /nonfatal /r "@CMAKE_INSTALL_PREFIX@\matlab"
+
   ;Store installation folder
   WriteRegStr HKCU "Software\${PACKAGE_NAME}" "" $INSTDIR
-  
+
   ;Create uninstaller
   WriteUninstaller "$INSTDIR\Uninstall.exe"
-  
+
   !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
-    
+
     ;Create shortcuts
     CreateDirectory "$SMPROGRAMS\$STARTMENU_FOLDER"
     CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\knut-gui.lnk" "$INSTDIR\bin\@GUI_NAME@.exe"
     CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\Uninstall.lnk" "$INSTDIR\Uninstall.exe"
-  
+
   !insertmacro MUI_STARTMENU_WRITE_END
 
 SectionEnd
@@ -113,7 +113,7 @@ SectionEnd
 ;  !insertmacro MUI_FUNCTION_DESCRIPTION_BEGIN
 ;    !insertmacro MUI_DESCRIPTION_TEXT ${SecDummy} $(DESC_SecDummy)
 ;  !insertmacro MUI_FUNCTION_DESCRIPTION_END
-; 
+;
 ;--------------------------------
 ;Uninstaller Section
 
@@ -129,22 +129,22 @@ Section "Uninstall"
   Delete "$INSTDIR\Uninstall.exe"
 
   RMDir "$INSTDIR"
-  
+
   !insertmacro MUI_STARTMENU_GETFOLDER Application $MUI_TEMP
-    
+
   Delete "$SMPROGRAMS\$MUI_TEMP\Uninstall.lnk"
   Delete "$SMPROGRAMS\$MUI_TEMP\knut-gui.lnk"
-  
+
   ;Delete empty start menu parent diretories
   StrCpy $MUI_TEMP "$SMPROGRAMS\$MUI_TEMP"
- 
+
   startMenuDeleteLoop:
 	ClearErrors
     RMDir $MUI_TEMP
     GetFullPathName $MUI_TEMP "$MUI_TEMP\.."
-    
+
     IfErrors startMenuDeleteLoopDone
-  
+
     StrCmp $MUI_TEMP $SMPROGRAMS startMenuDeleteLoopDone startMenuDeleteLoop
   startMenuDeleteLoopDone:
 
